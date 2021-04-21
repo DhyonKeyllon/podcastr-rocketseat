fd84401e55b85c1710adb1731552391af713659c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-04-21 14:06:18 -0300

    Dados formatados
    Formatei dados da  published_at e duration .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-// SPA 
-// SSR
-// SSG
+import { GetStaticProps } from 'next';
+import {format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  // ...
+}
 
+type HomeProps = {
+  episodes: Episode[];
+}
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -14,15 +26,34 @@ export default function Home(props) {
 }
 
 // entrega a mesma requisicao para todos os clientes, ou seja, não precisa fazer uma nova requisição toda vez que um cliente solicitar. Aumentando a perfomance do site. SSG (Static Site Generation)
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     // atualiza entrega de pagina de 8 em 8 horas
-    revalidate: 60 * 60 * 8,
+    revalidate: 60 * 60 * 8
   }
 }
